3d42441a105c7aeac67d642f855ab737a5a71e1a
phielp <phielp@gmail.com>
2017-10-13 12:00:44 +0200

    added lopen en drinken

--- Drinken.java
+++ Drinken.java
@@ -1,5 +1,15 @@
 public class Drinken {
 	public static void main(String[] args) {
-		System.out.println("Proost!")
+		drinken();
+		lopen(500);
+		drinken();
+	}
+
+	public static void lopen(int afstand) {
+		System.out.println("De volgende kroeg is " + afstand + " meter lopen.");
+	}
+
+	public static void drinken() {
+		System.out.println("Proost!");
 	}
 }
